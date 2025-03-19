8a83a60fbe82247b4c9340c1580747be73f96c9d
lyrics05 <dca49@hotmail.com>
2025-03-18 18:34:38 -0600

    rutas cambiadas

--- client/src/components/actions/index.js
+++ client/src/components/actions/index.js
@@ -4,7 +4,7 @@ import { GET_VIDEO_GAMES,GET_GENRES,GET_GAME_DETAILS,SEARCH_GAME,ALPHABETICAL_SO
 
 export function getVideoGames() {
     return function(dispatch) {
-     axios.get("http://localhost:3001/videogames")
+     axios.get("https://app-videigames.onrender.com/videogames")
     .then((response) => {
        dispatch({type:GET_VIDEO_GAMES,payload:response.data})
     }).catch((error) => {
@@ -16,7 +16,7 @@ export function getVideoGames() {
 export function getGamesById (id){
     
     return async function(dispatch){
-        var json = await axios.get(`http://localhost:3001/videogames/${id}`);
+        var json = await axios.get(`https://app-videigames.onrender.com/videogames/${id}`);
     return dispatch( {
         type : GET_GAME_DETAILS,
         payload: json.data
@@ -27,7 +27,7 @@ export function getGamesById (id){
 export function getGameByName(name) {
     return async function(dispatch) {
     
-         await axios.get(`http://localhost:3001/videogames?name=${name}`).then(res=>{
+         await axios.get(`https://app-videigames.onrender.com/videogames?name=${name}`).then(res=>{
             dispatch({type:SEARCH_GAME,payload:res.data})
         })
     }
@@ -74,7 +74,7 @@ export function createdTypeFilter(payload) {
 export function crearGame(payload){
     return async function(dispatch){
      try {
-        var response = await axios.post("http://localhost:3001/videogame",payload)
+        var response = await axios.post("https://app-videigames.onrender.com/videogame",payload)
         alert("CREADO")
         return response
      } catch (error) {
@@ -85,7 +85,7 @@ export function crearGame(payload){
 export function getGenres (){
     
     return async function(dispatch){
-        var json = await axios.get(`http://localhost:3001/genres`);
+        var json = await axios.get(`https://app-videigames.onrender.com/genres`);
        
         return dispatch( {
             type : GET_GENRES,
